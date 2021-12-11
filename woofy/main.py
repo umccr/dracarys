cac34a305997d854e0fa21cba23bdc1075221355
pdiakumis <peterdiakumis@gmail.com>
2021-12-12 00:51:49 +1100

    add bump2version config

--- woofy/main.py
+++ woofy/main.py
@@ -1,8 +1,8 @@
 import click
-from woofy import dog
+from woofy import dog, _version
 
 @click.group(context_settings=dict(help_option_names=['-h', '--help']))
-@click.version_option(version.__version__)
+@click.version_option(_version.__version__)
 def cli():
     """Somatic Workflow Postprocessing"""
 
