a5289c3cdd19df797c11db9eaf0c4c73d05a343d
pdiakumis <peterdiakumis@gmail.com>
2022-02-27 12:27:05 +1100

    use httr for ica api requests

--- inst/scripts/ica_download.R
+++ inst/scripts/ica_download.R
@@ -4,7 +4,9 @@ require(glue)
 require(here)
 require(fs)
 require(jsonlite)
+require(httr)
 
+# dragen wgs
 j <- here("nogit/seqcii/seqc_dragen_outputs.json")
 l <- jsonlite::read_json(j)[["items"]]
 p <- c("replay.json", "fragment_length_hist.csv",
@@ -26,3 +28,52 @@ bind_rows(l) |>
   select(gds, target) |>
   rowwise() |>
   mutate(cmd = system(glue("ica files download {gds} {target}")))
+
+# tso
+tso_download_ica <- function(sname) {
+  base_url <- "https://aps2.platform.illumina.com/v1"
+  path <- glue::glue("/analysis_data/{sname}/tso_ctdna_tumor_only/*")
+  token <- Sys.getenv("ICA_ACCESS_TOKEN")
+  volname <- "production"
+  r1 <- httr::GET(glue("{base_url}/files?volume.name={volname}&path={path}&pageSize=100"),
+                  httr::add_headers(Authorization = glue("Bearer {token}")),
+                  httr::accept_json())
+  parsed <- jsonlite::fromJSON(httr::content(r1, "text"), simplifyVector = FALSE)
+  ftypes <- c("AlignCollapseFusionCaller_metrics", "TargetRegionCoverage", "MSI",
+              "FragmentLengthHist",  "CombinedVariantOutput",
+              "TMB",  "TMBtrace_json", "TMBtrace_tsv",
+              "FailedExonCoverageQC_json", "FailedExonCoverageQC_txt",
+              "Fusions_csv", "Fusions_json", "SampleAnalysisResults")
+
+  map_df(parsed[["items"]], function(x) c(path = x[["path"]], size = x[["sizeInBytes"]])) |>
+    mutate(
+      size = fs::as_fs_bytes(size),
+      bname = basename(path),
+      type = tso_guess_file_type(bname),
+      path = glue("gds://{volname}{path}"),
+      sample = sname) |>
+    filter(type %in% ftypes) |>
+    mutate(dname = basename(dirname(path))) |>
+    select(sample, dname, type, size, path, bname)
+}
+
+samples <- c(
+  "SBJ00998", "SBJ00999", "SBJ01001", "SBJ01003", "SBJ01032", "SBJ01040",
+  "SBJ01043", "SBJ01046", "SBJ01059", "SBJ01131", "SBJ01132", "SBJ01133",
+  "SBJ01134", "SBJ01135", "SBJ01136", "SBJ01137", "SBJ01138", "SBJ01139",
+  "SBJ01140", "SBJ01141", "SBJ01142", "SBJ01143", "SBJ01144", "SBJ01146",
+  "SBJ01150", "SBJ01156", "SBJ01157", "SBJ01158", "SBJ01159", "SBJ01160",
+  "SBJ01162", "SBJ01163", "SBJ01164", "SBJ01195", "SBJ01196", "SBJ01197",
+  "SBJ01211", "SBJ01212", "SBJ01213", "SBJ01214", "SBJ01215", "SBJ01216",
+  "SBJ01217", "SBJ01218", "SBJ01219", "SBJ01220", "SBJ01221", "SBJ01222",
+  "SBJ01223", "SBJ01224"
+)
+
+# 13 files * 50 samples = 650 files
+d <- map_df(samples, tso_download_ica)
+outdir <- here::here("nogit/tso")
+d |>
+  mutate(out = file.path(outdir, sample, bname)) |>
+  rowwise() |>
+  mutate(
+    cmd = system(glue("ica files download {path} {out}")))
