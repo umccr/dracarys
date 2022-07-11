59c300e9e3c057197b0d9b4d0bd083d852bea6aa
pdiakumis <peterdiakumis@gmail.com>
2022-07-11 15:54:12 +1000

    update conda recipe

--- conda/recipe/build.sh
+++ conda/recipe/build.sh
@@ -1,4 +1,9 @@
-#!/usr/bin/env bash
-set -x
+#!/bin/bash
 
-$PYTHON -m pip install . --no-deps -vv
+export DISABLE_AUTOBREW=1
+${R} CMD INSTALL --build . ${R_ARGS}
+
+# Copy CLI to conda bin
+mkdir -p ${PREFIX}/bin
+cp ${SRC_DIR}/inst/cli/dracarys.R ${PREFIX}/bin
+chmod +x ${PREFIX}/bin/dracarys.R
