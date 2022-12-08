b25132cf1df11c8897caed609037295c3541633a
pdiakumis <peterdiakumis@gmail.com>
2022-12-08 17:33:00 +1100

    add gds file list/download funcs

---
+++ R/ica.R
@@ -0,0 +1,41 @@
+#' GDS File Download
+#'
+#' @param gds Full path to GDS file.
+#' @param out Path to output file.
+#' @param token ICA access token (by default uses $ICA_ACCESS_TOKEN env var).
+#' @export
+gds_file_download <- function(gds, out, token = Sys.getenv("ICA_ACCESS_TOKEN")) {
+  system(glue::glue("ica files download {gds} {out} --access-token {token}"))
+}
+
+#' GDS Files List
+#'
+#' List files on ICA GDS file system.
+#'
+#' @param gdsdir Full path to GDS directory, must end with `/`.
+#' @param token ICA access token (by default uses $ICA_ACCESS_TOKEN env var).
+#'
+#' @return Tibble with file basename, file size, file full data path, file dir name.
+#' @export
+gds_files_list <- function(gdsdir, token = Sys.getenv("ICA_ACCESS_TOKEN")) {
+  assertthat::assert_that(grepl("^gds://", gdsdir), grepl("/$", gdsdir))
+  base_url <- "https://aps2.platform.illumina.com/v1"
+  volname <- sub("gds://(.*?)/.*", "\\1", gdsdir)
+  path2 <- sub("gds://(.*?)/(.*)", "\\2", gdsdir)
+
+  res <- httr::GET(
+    glue::glue("{base_url}/files?volume.name={volname}&path=/{path2}*&pageSize=100"),
+    httr::add_headers(Authorization = glue::glue("Bearer {token}")),
+    httr::accept_json()
+  )
+  j <- jsonlite::fromJSON(httr::content(res, "text"), simplifyVector = FALSE)[["items"]]
+  d <- purrr::map_df(j, function(x) c(path = x[["path"]], size = x[["sizeInBytes"]]))
+  d |>
+    dplyr::mutate(
+      size = fs::as_fs_bytes(.data$size),
+      bname = basename(.data$path),
+      path = glue::glue("gds://{volname}{.data$path}"),
+      dname = basename(dirname(.data$path))
+    ) |>
+    dplyr::select(.data$bname, .data$size, .data$path, .data$dname)
+}
