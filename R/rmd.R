5d026bb576929c42c3f1f642df4918ccd57da926
pdiakumis <peterdiakumis@gmail.com>
2023-01-05 12:41:38 +1100

    tso_rmd: use rds as preeval func input

--- R/rmd.R
+++ R/rmd.R
@@ -1,46 +1,36 @@
 #' Generate TSO Report
 #'
 #' Generates a HTML report with results from the UMCCR TSO500 ctDNA workflow.
-#'
-#' @inheritParams tso_tidy
+#' @param tidy_rds Path to RDS object output from `tso_tidy`.
+#' @param out_dir Output directory.
+#' @param prefix Prefix of output file(s).
 #' @param quiet Suppress warning and other messages.
-#' @param token ICA access token (by default uses $ICA_ACCESS_TOKEN env var).
 #'
 #' @return Path to rendered HTML report.
 #' @examples
 #' \dontrun{
-#' in_dir <- here::here(glue("nogit/tso/2022-12-13/SBJ02858/dracarys_gds_sync"))
-#' gds_local_dir <- NULL
-#' out_format <- "tsv"
+#' tidy_rds <- "~/tmp/tso/SBJ02858.rds"
+#' tso_tidy(...) |> saveRDS(tidy_rds)
 #' quiet <- TRUE
-#' out_dir <- file.path(in_dir, "../out")
+#' out_dir <- file.path("~/tmp/tso/out2")
 #' prefix <- "SBJ02858"
-#' tso_rmd(in_dir = in_dir, out_dir = out_dir, prefix = prefix, quiet = quiet)
+#' tso_rmd(tidy_rds = tidy_rds, out_dir = out_dir, prefix = prefix, quiet = quiet)
 #' }
 #' @export
-tso_rmd <- function(in_dir, out_dir, prefix, gds_local_dir = NULL, out_format = "tsv",
-                    token = Sys.getenv("ICA_ACCESS_TOKEN"), quiet = FALSE) {
-  assertthat::assert_that(quiet %in% c(FALSE, TRUE))
+tso_rmd <- function(tidy_rds, out_dir, prefix, quiet = FALSE) {
+  assertthat::assert_that(
+    rlang::is_logical(quiet), file.exists(tidy_rds), fs::path_ext(tidy_rds) == "rds"
+  )
   tmp_dir <- tempdir()
   rmd_dir <- system.file("rmd/tso", package = "dracarys")
   fs::dir_copy(rmd_dir, tmp_dir, overwrite = TRUE)
   rmd_file <- file.path(tmp_dir, "tso.Rmd")
-  out_dir_rds <- file.path(out_dir, "rds")
-  out_dir_html <- file.path(out_dir, "html")
-  out_dir_dr <- file.path(out_dir, "dracarys")
-  c(out_dir_dr, out_dir_rds, out_dir_html) |> fs::dir_create()
-  dat <- tso_tidy(
-    in_dir = in_dir, out_dir = out_dir_dr, prefix = prefix, gds_local_dir = gds_local_dir,
-    out_format = out_format, dryrun = FALSE, token = token
-  )
-  dat_rds <- file.path(out_dir_rds, glue("{prefix}_tso_dracarys_data.rds"))
-  readr::write_rds(dat, dat_rds)
+  fs::dir_create(out_dir)
   out_html <- glue("{prefix}_tso_dracarys_report.html")
   # suppress DT large size warning
   options(DT.warn.size = FALSE)
-  pars <- list(rds = dat_rds)
   rmarkdown::render(
-    input = rmd_file, output_dir = out_dir_html, output_file = I(out_html),
-    params = pars, quiet = quiet
+    input = rmd_file, output_dir = out_dir, output_file = I(out_html),
+    params = list(rds = tidy_rds), quiet = quiet
   )
 }
