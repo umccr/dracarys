fd36505d1a12c121e183c808b1faf00d215cb04a
pdiakumis <peterdiakumis@gmail.com>
2023-02-21 15:53:54 +1100

    README: more usage/installation info

--- inst/cli/tidy.R
+++ inst/cli/tidy.R
@@ -1,12 +1,12 @@
 tidy_add_args <- function(subp) {
   tidy <- subp$add_parser("tidy", help = "Tidy UMCCR Workflow Outputs")
-  tidy$add_argument("-i", "--in_dir", help = glue("{emoji('snowman')} Directory with untidy UMCCR workflow results (GDS or local)."), required = TRUE)
+  tidy$add_argument("-i", "--in_dir", help = glue("{emoji('snowman')} Directory with untidy UMCCR workflow results. Can be GDS or local."), required = TRUE)
   tidy$add_argument("-o", "--out_dir", help = glue("{emoji('fire')} Directory to output tidy results."), required = TRUE)
-  tidy$add_argument("-p", "--prefix", help = glue("{emoji('violin')} Prefix string (used for all results)."), required = TRUE)
-  tidy$add_argument("-t", "--token", help = glue("{emoji('see_no_evil')} ICA access token (def. ICA_ACCESS_TOKEN env var)."), default = Sys.getenv("ICA_ACCESS_TOKEN"))
-  tidy$add_argument("-g", "--gds_local_dir", help = glue("{emoji('clipboard')} If input is a GDS directory, download the 'recognisable' files to this directory. If not specified, files will be downloaded to '<out_dir>/dracarys_gds_sync'."))
-  tidy$add_argument("-f", "--format", help = glue("{emoji('icecream')} Format of output (default: %(default)s)."), default = "tsv", choices = c("tsv", "parquet", "both"))
-  tidy$add_argument("-n", "--dryrun", help = glue("{emoji('camel')} Dry run (just show files to be tidied)."), action = "store_true")
+  tidy$add_argument("-p", "--prefix", help = glue("{emoji('violin')} Prefix string used for all results."), required = TRUE)
+  tidy$add_argument("-t", "--token", help = glue("{emoji('see_no_evil')} ICA access token. Default: ICA_ACCESS_TOKEN env var."), default = Sys.getenv("ICA_ACCESS_TOKEN"))
+  tidy$add_argument("-g", "--gds_local_dir", help = glue("{emoji('inbox_tray')} If input is a GDS directory, download the recognisable files to this directory. Default: '<out_dir>/dracarys_gds_sync'."))
+  tidy$add_argument("-f", "--format", help = glue("{emoji('icecream')} Format of output. Default: %(default)s."), default = "tsv", choices = c("tsv", "parquet", "both"))
+  tidy$add_argument("-n", "--dryrun", help = glue("{emoji('camel')} Dry run - just show files to be tidied."), action = "store_true")
   tidy$add_argument("-q", "--quiet", help = glue("{emoji('sleeping')} Shush all the logs."), action = "store_true")
 }
 
