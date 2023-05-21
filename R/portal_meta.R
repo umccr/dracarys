dd0cb3ebadbeec4ccaf7ed337cf0cc7ebcaf0498
pdiakumis <peterdiakumis@gmail.com>
2023-05-22 01:02:51 +1000

    alignqc meta parser

--- R/portal_meta.R
+++ R/portal_meta.R
@@ -1,3 +1,49 @@
+#' Metadata for wgs align-qc workflow
+#'
+#' @param status Workflow status to keep (default: Succeeded).
+#' @param pmeta Portal workflows metadata table in TSV format.
+#'
+#' @return A tibble with metadata per wgs align-qc workflow run.
+#' @examples
+#' \dontrun{
+#' pmeta <- here::here("nogit/data_portal/2023-05-21_workflows.csv")
+#' wgs_alignqc_metadata(pmeta)
+#' }
+#' @export
+wgs_alignqc_metadata <- function(pmeta, status = "Succeeded") {
+  wf <- readr::read_csv(pmeta) |>
+    dplyr::filter(.data$type_name == "wgs_alignment_qc") |>
+    dplyr::select(-c("sample_name", "type_name", "notified", "partition_name")) |>
+    dplyr::filter(.data$end_status %in% status)
+
+  d <- wf |>
+    dplyr::rowwise() |>
+    dplyr::mutate(
+      i1 = list(jsonlite::fromJSON(.data$input)),
+      o1 = list(jsonlite::fromJSON(.data$output))
+    ) |>
+    dplyr::ungroup() |>
+    dplyr::mutate(
+      libid = purrr::map_chr(.data$i1, list("fastq_list_rows", "rglb")),
+      rgsm = purrr::map_chr(.data$i1, list("fastq_list_rows", "rgsm")),
+      outdir_alignqc = purrr::map_chr(.data$o1, list("dragen_alignment_output_directory", "location")),
+      outdir_multiqc = purrr::map_chr(.data$o1, list("multiqc_output_directory", "location")),
+      subjectid = sub("umccr__automated__wgs_alignment_qc__(SBJ.*)__L.*", "\\1", .data$wfr_name),
+      libid2 = sub("umccr__automated__wgs_alignment_qc__SBJ.*__(L.*)__.*", "\\1", .data$wfr_name),
+      lane = sub(".*__(\\d+)", "\\1", .data$libid2),
+      libid3 = sub("(.*)__\\d+", "\\1", .data$libid2)
+    ) |>
+    dplyr::select(
+      SubjectID = "subjectid",
+      LibraryID = "libid",
+      Lane = "lane",
+      SampleID = "rgsm",
+      "wfr_name", "wfr_id", "version", "sequence_run_id", "batch_run_id",
+      "start", "end", "portal_run_id", "outdir_alignqc", "outdir_multiqc"
+    )
+  d
+}
+
 #' Metadata for cttso workflow
 #'
 #' @param status Workflow status to keep (default: Succeeded).
