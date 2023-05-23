412eb5c65053e719fb661a1c73f80bef742e9625
pdiakumis <peterdiakumis@gmail.com>
2023-05-23 19:29:36 +1000

    refactor tso_ctdna_tumor_only meta parser

--- R/portal_meta.R
+++ R/portal_meta.R
@@ -26,7 +26,7 @@ meta_wgs_alignment_qc <- function(pmeta, status = "Succeeded") {
     dplyr::mutate(
       libid = purrr::map_chr(.data$i1, list("fastq_list_rows", "rglb")),
       rgsm = purrr::map_chr(.data$i1, list("fastq_list_rows", "rgsm")),
-      outdir_alignqc = purrr::map_chr(.data$o1, list("dragen_alignment_output_directory", "location")),
+      outdir_dragen = purrr::map_chr(.data$o1, list("dragen_alignment_output_directory", "location")),
       outdir_multiqc = purrr::map_chr(.data$o1, list("multiqc_output_directory", "location")),
       subjectid = sub("umccr__automated__wgs_alignment_qc__(SBJ.*)__L.*", "\\1", .data$wfr_name),
       libid2 = sub("umccr__automated__wgs_alignment_qc__SBJ.*__(L.*)__.*", "\\1", .data$wfr_name),
@@ -38,58 +38,88 @@ meta_wgs_alignment_qc <- function(pmeta, status = "Succeeded") {
       Lane = "lane",
       SampleID = "rgsm",
       "wfr_name", "wfr_id", "version", "sequence_run_id", "batch_run_id",
-      "start", "end", "portal_run_id", "outdir_alignqc", "outdir_multiqc"
+      "start", "end", "portal_run_id", "outdir_dragen", "outdir_multiqc"
     )
   d
 }
 
-#' Metadata for cttso workflow
+#' Metadata for tso_ctdna_tumor_only workflow
 #'
+#' @param pmeta Path to portal workflows metadata table, or tibble with already parsed data.
 #' @param status Workflow status to keep (default: Succeeded).
-#' @param pmeta Portal workflows metadata table in TSV format.
 #'
 #' @return A tibble with metadata per cttso workflow run.
 #' @examples
 #' \dontrun{
-#' pmeta <- here::here("nogit/data_portal/2023-05-07_workflows_5f43da12-0b6a-41ce-86c9-9ee62df8792e.csv")
-#' cttso_metadata(pmeta)
+#' pmeta <- here::here("nogit/data_portal/2023-05-21_workflows.csv")
+#' meta_tso_ctdna_tumor_only(pmeta)
 #' }
 #' @export
-cttso_metadata <- function(pmeta, status = c("Succeeded")) {
-  # retrieve successful cttso workflow runs
-  wf <- readr::read_csv(pmeta) |>
-    dplyr::filter(.data$type_name == "tso_ctdna_tumor_only") |>
-    dplyr::select(-c("sample_name", "type_name", "notified", "partition_name")) |>
-    dplyr::filter(.data$end_status %in% dplyr::all_of(status))
-
+meta_tso_ctdna_tumor_only <- function(pmeta, status = c("Succeeded")) {
+  # retrieve cttso workflow runs with the given status (Succeeded by default)
+  wf <- portal_meta_read(pmeta) |>
+    dplyr::filter(
+      .data$type_name == "tso_ctdna_tumor_only",
+      .data$end_status %in% dplyr::all_of(status)
+    )
   # grab libid/sampleid from the input meta, and outdir from the output meta
   d <- wf |>
-    dplyr::rowwise() |>
+    meta_io_fromjson() |>
     dplyr::mutate(
-      i1 = list(jsonlite::fromJSON(.data$input)),
-      o1 = list(jsonlite::fromJSON(.data$output))
-    ) |>
-    dplyr::ungroup() |>
-    dplyr::mutate(
-      tso500_samples = purrr::map(.data$i1, "tso500_samples"),
-      outdir = purrr::map(.data$o1, list("output_results_dir", "location"))
-    ) |>
-    tidyr::unnest(.data$tso500_samples) |>
-    dplyr::mutate(
+      sample_id = purrr::map_chr(.data$input, list("tso500_samples", "sample_id")),
+      sample_name2 = purrr::map_chr(.data$input, list("tso500_samples", "sample_name")),
+      gds_outdir = purrr::map_chr(.data$output, list("output_results_dir", "location")),
       libid1 = sub(".*_(L.*)", "\\1", .data$sample_id),
       rerun = grepl("rerun", .data$libid1),
       subjectid = sub("umccr__automated__tso_ctdna_tumor_only__(SBJ.*)__L.*", "\\1", .data$wfr_name),
       libid = sub("umccr__automated__tso_ctdna_tumor_only__SBJ.*__(L.*)__.*", "\\1", .data$wfr_name) # equal to libid1 wo _rerun
-    ) |>
-    tidyr::unnest(.data$outdir) |>
+    )
+  d |>
     dplyr::select(
+      tidyselect::all_of(meta_main_cols()),
       SubjectID = "subjectid",
       LibraryID = "libid",
-      LibraryID_w_rerun = "libid1",
-      SampleID = "sample_name",
-      gds_indir = "outdir",
-      "wfr_name", "wfr_id", "version", "sequence_run_id", "batch_run_id",
-      "start", "end", "portal_run_id", "rerun"
+      SampleID = "sample_name2",
+      gds_outdir,
+      cttso_rerun = "rerun"
     )
-  d
+}
+
+meta_io_fromjson <- function(pmeta) {
+  pmeta <- portal_meta_read(pmeta)
+  pmeta |>
+    dplyr::rowwise() |>
+    dplyr::mutate(
+      input = list(jsonlite::fromJSON(.data$input)),
+      output = list(jsonlite::fromJSON(.data$output))
+    ) |>
+    dplyr::ungroup()
+}
+
+portal_meta_read <- function(pmeta) {
+  # if already parsed, just return it
+  if (inherits(pmeta, "data.frame")) {
+    assertthat::assert_that(
+      all(c(
+        "wfr_name", "type_name", "start", "end", "input", "output",
+        "portal_run_id", "wfr_id", "wfl_id", "wfv_id", "version", "end_status"
+      ) %in% colnames(pmeta))
+    )
+    return(pmeta)
+  }
+  assertthat::assert_that(file.exists(pmeta))
+  # keep all character except start/end
+  ctypes <- readr::cols(
+    .default = "c",
+    start = readr::col_datetime(format = ""),
+    end = readr::col_datetime(format = ""),
+  )
+  readr::read_csv(pmeta, col_types = ctypes)
+}
+
+meta_main_cols <- function() {
+  c(
+    "id", "wfr_name", "wfr_id", "version", "sequence_run_id", "batch_run_id",
+    "start", "end", "portal_run_id"
+  )
 }
