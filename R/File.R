b3aa38596c391dbdc40ee5bd2e6e5fe8d05f8b47
pdiakumis <peterdiakumis@gmail.com>
2023-06-02 15:15:29 +1000

    File: dispatch read/write methods based on file type

--- R/File.R
+++ R/File.R
@@ -3,20 +3,18 @@
 #' @description File is a base R6 class representing a TSV/CSV/JSON output from
 #' a DRAGEN workflow.
 #'
-#' A File has a path, a basename, a type, and a default read method for its type.
+#' A File has a path, a basename and a type.
 #'
 #' @examples
 #' F1 <- File$new(readr::readr_example("mtcars.csv"))
-#' (parsed_f1 <- F1$read(col_types = readr::cols("double")))
 #' (bname_f1 <- F1$bname())
 #' (F2 <- File$new("https://stratus-gds-aps2/foo/bar/baz.csv?bla"))
 #'
 #' @testexamples
 #' expect_true(inherits(F1, c("File", "R6")))
-#' expect_true(inherits(parsed_f1, "data.frame"))
 #' expect_equal(bname_f1, "mtcars.csv")
 #' expect_equal(F2$bname(), "baz.csv")
-#' expect_equal(F2$type(), "CSV")
+#' expect_equal(F2$type(), NA_character_)
 #'
 #' @export
 File <- R6::R6Class("File", public = list(
@@ -41,19 +39,10 @@ File <- R6::R6Class("File", public = list(
   },
 
   #' @description Get the type of file.
-  #' @return String describing the specific type of file (fallbacks are CSV, TSV, JSON or OTHER).
+  #' @return String describing the specific type of dracarys file (NA if not a dracarys-recognised file).
   type = function() {
     nm <- self$bname()
-    x <- match_regex(nm)
-    if (!is.na(x)) {
-      return(x)
-    }
-    dplyr::case_when(
-      grepl("\\.json", nm) ~ "JSON",
-      grepl("\\.csv", nm) ~ "CSV",
-      grepl("\\.tsv", nm) ~ "TSV",
-      TRUE ~ "OTHER"
-    )
+    match_regex(nm)
   },
 
   #' @description Print details about the File.
@@ -66,22 +55,21 @@ File <- R6::R6Class("File", public = list(
     invisible(self)
   },
 
-  #' @description Read the file based on its type.
-  #' @param ... Arguments passed on to appropriate read_* function.
+  #' @description Read the file based on its type. Returns NULL if not a
+  #' dracarys-recognised file.
+  #' @param ... Arguments passed on to appropriate read function.
   read = function(...) {
     x <- self$path
     t <- self$type()
-    possible_types <- c("CSV", "TSV", "JSON", "OTHER")
-    assertthat::assert_that(t %in% possible_types)
-    if (t == "CSV") {
-      readr::read_csv(x, ...)
-    } else if (t == "TSV") {
-      readr::read_tsv(x, ...)
-    } else if (t == "JSON") {
-      jsonlite::read_json(x, ...)
-    } else {
-      stop(glue("Don't know how to read file of type {t}."))
-    }
+    func_selector(t)$new(x)$read(...)
+  },
+  #' @description Write the file based on its type. Returns NULL if not a
+  #' dracarys-recognised file.
+  #' @param ... Arguments passed on to appropriate write function.
+  write = function(...) {
+    x <- self$path
+    t <- self$type()
+    func_selector(t)$new(x)$write(...)
   }
 ))
 
