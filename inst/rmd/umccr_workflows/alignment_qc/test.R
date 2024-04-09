e9aa548fe252668c36b15c42713ae0a20cae30ed
pdiakumis <peterdiakumis@gmail.com>
2024-04-09 11:50:01 +1000

    moved meta tidiers + db queries to rportal

--- inst/rmd/umccr_workflows/alignment_qc/test.R
+++ inst/rmd/umccr_workflows/alignment_qc/test.R
@@ -1,13 +1,10 @@
 #!/usr/bin/env Rscript
 
 {
-  require(DBI)
   require(dplyr)
   require(dracarys)
-  require(fs)
-  require(glue)
-  require(here)
-  require(RAthena)
+  require(glue, include.only = "glue")
+  require(here, include.only = "here")
   require(readr)
   require(rportal, include.only = c("awsvault_profile"))
 }
@@ -15,60 +12,38 @@
 # log into aws umccr prod account
 rportal::awsvault_profile("upro")
 
-dp_workflow_read <- function(start_date) {
-  RAthena::RAthena_options(clear_s3_resource = FALSE)
-  con <- DBI::dbConnect(
-    RAthena::athena(),
-    work_group = "data_portal",
-    rstudio_conn_tab = FALSE
-  )
+query_workflow_alignqc <- function(start_date) {
   q1 <- glue(
-    'SELECT * FROM "data_portal"."data_portal"."data_portal_workflow" ',
     'WHERE "start" > date(\'{start_date}\') AND REGEXP_LIKE("type_name", \'alignment_qc\') ',
     'ORDER BY "start" DESC;'
   )
-  d <- RAthena::dbGetQuery(con, q1) |>
-    tibble::as_tibble()
-  DBI::dbDisconnect(con)
-  d
+  rportal::portaldb_query_workflow(q1)
 }
 
-# TODO: use dracarys::glims_portal_read
-dp_lims_read <- function(libids) {
+query_limsrow_libids <- function(libids) {
   assertthat::assert_that(!is.null(libids), all(grepl("^L", libids)))
   libids <- unique(libids)
-  RAthena::RAthena_options(clear_s3_resource = FALSE)
-  con <- DBI::dbConnect(
-    RAthena::athena(),
-    work_group = "data_portal",
-    rstudio_conn_tab = FALSE
-  )
   q_quote <- shQuote(paste(libids, collapse = "|"))
-  q1 <- glue(
-    'SELECT * FROM "data_portal"."data_portal"."data_portal_limsrow" where REGEXP_LIKE("library_id", {q_quote});'
-  )
-  d <- RAthena::dbGetQuery(con, q1) |>
-    tibble::as_tibble()
-  DBI::dbDisconnect(con)
-  d
+  q1 <- glue('WHERE REGEXP_LIKE("library_id", {q_quote});')
+  rportal::portaldb_query_limsrow(q1)
 }
 
 # first read in the workflows table, extract metadata, then join with lims
 start_date <- "2024-03-23"
-p_raw <- dp_workflow_read(start_date)
+p_raw <- query_workflow_alignqc(start_date)
 
 wgs <- p_raw |>
   filter(type_name == "wgs_alignment_qc") |>
-  dracarys::meta_wgs_alignment_qc(status = "Succeeded")
+  rportal::meta_wgs_alignment_qc(status = "Succeeded")
 wts <- p_raw |>
   filter(type_name == "wts_alignment_qc") |>
-  dracarys::meta_wts_alignment_qc(status = "Succeeded")
-p <- dplyr::bind_rows(wgs, wts)
-lims <- dp_lims_read(p$LibraryID)
+  rportal::meta_wts_alignment_qc(status = "Succeeded")
+p <- bind_rows(wgs, wts)
+lims <- query_limsrow_libids(p$LibraryID)
 
 d <- p |>
-  dplyr::left_join(lims, by = c("SubjectID" = "subject_id", "LibraryID" = "library_id")) |>
-  dplyr::select(
+  left_join(lims, by = c("SubjectID" = "subject_id", "LibraryID" = "library_id")) |>
+  select(
     SubjectID, LibraryID, SampleID, Lane, phenotype, type, source,
     quality, assay, external_subject_id, project_name, project_owner,
     workflow, start, end, portal_run_id, gds_outdir_dragen
@@ -76,15 +51,15 @@ d <- p |>
 
 tidy_script <- system.file("cli/dracarys.R", package = "dracarys")
 meta <- d |>
-  dplyr::rowwise() |>
-  dplyr::mutate(
+  rowwise() |>
+  mutate(
     indir = gds_outdir_dragen,
     outdir = file.path(sub("gds://", "", .data$indir)),
     outdir = file.path(normalizePath("~/icav1/g"), .data$outdir),
     # indir = file.path(outdir, "dracarys_gds_sync"), # for when debugging locally
-    cmd = system(glue::glue("{tidy_script} tidy --in_dir {.data$indir} --out_dir {.data$outdir} --prefix {.data$SampleID} --format rds"))
+    cmd = system(glue("{tidy_script} tidy --in_dir {.data$indir} --out_dir {.data$outdir} --prefix {.data$SampleID} --format rds"))
   ) |>
-  dplyr::ungroup()
+  ungroup()
 
 meta |>
-  saveRDS(here::here(glue::glue("inst/rmd/umccr_workflows/alignment_qc/nogit/meta/{start_date}_wgts.rds")))
+  saveRDS(here(glue("inst/rmd/umccr_workflows/alignment_qc/nogit/meta/{start_date}_wgts.rds")))
