e439b4a6d9e3e63854cd6ee9483b1865790d1385
pdiakumis <peterdiakumis@gmail.com>
2024-06-25 08:22:52 +1000

    bclc: move readers into class

--- R/bclconvert.R
+++ R/bclconvert.R
@@ -1,3 +1,41 @@
+#' bcl_convert Wf R6 Class
+#'
+#' @description
+#' Contains methods for reading and processing files output from the UMCCR
+#' `bcl_convert` workflow.
+#'
+#' @examples
+#' \dontrun{
+#' indir <- file.path(
+#'   "~/icav1/g/production/analysis_data/SBJ00596/tso_ctdna_tumor_only",
+#'   "2024050555972acf/L2400482/Results/PTC_ctTSO240429_L2400482/dracarys_gds_sync"
+#' )
+#' sample_id <- "PTC_ctTSO240429"
+#' library_id <- "L2400482"
+#' d <- TsoCombinedVariantOutputFile$new(x)
+#' d$read()
+#' }
+#' @export
+Wf_bcl_convert <- R6::R6Class(
+  "Wf_bcl_convert",
+  public = list(
+    #' @field indir Input directory containing Reports per assay type
+    #' (e.g. /primary_data/240607_A01052_0209_BHLHFTDSXC/2024061140802544/).
+    indir = NULL,
+
+    #' @description Create a new Wf_bcl_convert object.
+    #' @param indir Input directory containing Reports per assay type.
+    initialize = function(indir) {
+      self$indir <- indir
+    },
+    #' @description Print details about the Workflow
+    #' @param ... (ignored).
+    print = function(...) {
+
+    }
+  )
+)
+
 #' BclconvertReports R6 Class
 #'
 #' @description
@@ -67,10 +105,66 @@ BclconvertReports <- R6::R6Class(
       assertthat::assert_that(
         all(req_fnames %in% self$contents[["bname"]])
       )
-      am <- bclconvert_read_adaptermetrics(file.path(p, "Adapter_Metrics.csv"))
-      ds <- bclconvert_read_demultiplexstats(file.path(p, "Demultiplex_Stats.csv"))
-      ih <- bclconvert_read_indexhoppingcounts(file.path(p, "Index_Hopping_Counts.csv"))
-      ub <- bclconvert_read_topunknownbarcodes(file.path(p, "Top_Unknown_Barcodes.csv"))
+      .read_topunknownbarcodes <- function(x) {
+        d <- readr::read_csv(x, col_types = "cccd")
+        assertthat::assert_that(all(colnames(d) == c("Lane", "index", "index2", "# Reads")))
+        d |>
+          rlang::set_names(c("lane", "index1", "index2", "n_reads")) |>
+          dplyr::mutate(barcode = glue("{.data$index1}-{.data$index2}") |> as.character()) |>
+          dplyr::select("lane", "barcode", "n_reads")
+      }
+      .read_adaptermetrics <- function(x) {
+        d <- readr::read_csv(x, col_types = "ccccddddd")
+        old_nms <- c(
+          "Lane", "Sample_ID", "index", "index2", "R1_AdapterBases",
+          "R1_SampleBases", "R2_AdapterBases", "R2_SampleBases", "# Reads"
+        )
+        assertthat::assert_that(all(colnames(d) == old_nms))
+        d |>
+          dplyr::rename(
+            index1 = "index", n_reads = "# Reads", SampleID = "Sample_ID", lane = "Lane"
+          ) |>
+          dplyr::mutate(barcode = ifelse(
+            is.na(.data$index1), NA_character_, glue("{.data$index1}-{.data$index2}")
+          )) |>
+          dplyr::select(
+            "lane", "SampleID", "barcode", "n_reads",
+            "R1_AdapterBases", "R2_AdapterBases",
+            "R1_SampleBases", "R2_SampleBases"
+          )
+      }
+      .read_indexhoppingcounts <- function(x) {
+        d <- readr::read_csv(x, col_types = "ccccd")
+        old_nms <- c("Lane", "SampleID", "index", "index2", "# Reads")
+        assertthat::assert_that(all(colnames(d) == old_nms))
+        d |>
+          dplyr::rename(index1 = "index", n_reads = "# Reads", lane = "Lane") |>
+          dplyr::mutate(barcode = glue("{.data$index1}-{.data$index2}")) |>
+          dplyr::select("lane", "SampleID", "barcode", "n_reads")
+      }
+      .read_demultiplexstats <- function(x) {
+        nms <- tibble::tribble(
+          ~new_nm, ~old_nm, ~class,
+          "lane", "Lane", "c",
+          "SampleID", "SampleID", "c",
+          "barcode", "Index", "c",
+          "n_reads", "# Reads", "d",
+          "n_perfect_idxreads", "# Perfect Index Reads", "d",
+          "n_one_mismatch_idxreads", "# One Mismatch Index Reads", "d",
+          "n_q30_bases", "# of >= Q30 Bases (PF)", "d",
+          "mean_quality_score", "Mean Quality Score (PF)", "d"
+        )
+        lookup <- tibble::deframe(nms[c("new_nm", "old_nm")])
+        d <- readr::read_csv(x, col_types = nms[["class"]])
+        assertthat::assert_that(all(colnames(d) == nms[["old_nm"]]))
+        d |>
+          dplyr::rename(dplyr::all_of(lookup))
+      }
+
+      am <- .read_adaptermetrics(file.path(p, "Adapter_Metrics.csv"))
+      ds <- .read_demultiplexstats(file.path(p, "Demultiplex_Stats.csv"))
+      ih <- .read_indexhoppingcounts(file.path(p, "Index_Hopping_Counts.csv"))
+      ub <- .read_topunknownbarcodes(file.path(p, "Top_Unknown_Barcodes.csv"))
       list(
         adapter_metrics = am,
         demultiplex_stats = ds,
@@ -78,6 +172,7 @@ BclconvertReports <- R6::R6Class(
         top_unknown_barcodes = ub
       )
     },
+
     #' @description
     #' Writes tidied contents of `Reports` directory output by BCLConvert.
     #'
@@ -105,122 +200,3 @@ BclconvertReports <- R6::R6Class(
     }
   )
 )
-
-#' BCLConvert Top Unknown Barcodes
-#'
-#' Reads the `Top_Unknown_Barcodes.csv` file in the `Reports` directory
-#' output by BCLConvert.
-#'
-#' @param x Path to `Top_Unknown_Barcodes.csv` file.
-#'
-#' @return Tibble
-#'
-#' @examples
-#' \dontrun{
-#' x <- here::here("nogit/bcl_convert/WGS_TsqNano/Reports/Top_Unknown_Barcodes.csv")
-#' bclconvert_read_topunknownbarcodes(x)
-#' }
-#' @export
-bclconvert_read_topunknownbarcodes <- function(x) {
-  d <- readr::read_csv(x, col_types = "cccd")
-  assertthat::assert_that(all(colnames(d) == c("Lane", "index", "index2", "# Reads")))
-  d |>
-    rlang::set_names(c("lane", "index1", "index2", "n_reads")) |>
-    dplyr::mutate(barcode = glue("{.data$index1}-{.data$index2}") |> as.character()) |>
-    dplyr::select("lane", "barcode", "n_reads")
-}
-
-#' BCLConvert Adapter Metrics
-#'
-#' Reads the `Adapter_Metrics.csv` file in the `Reports` directory
-#' output by BCLConvert.
-#'
-#' @param x Path to `Adapter_Metrics.csv` file.
-#'
-#' @return Tibble
-#'
-#' @examples
-#' \dontrun{
-#' x <- here::here("nogit/bcl_convert/WGS_TsqNano/Reports/Adapter_Metrics.csv")
-#' bclconvert_read_adaptermetrics(x)
-#' }
-#' @export
-bclconvert_read_adaptermetrics <- function(x) {
-  d <- readr::read_csv(x, col_types = "ccccddddd")
-  old_nms <- c(
-    "Lane", "Sample_ID", "index", "index2", "R1_AdapterBases",
-    "R1_SampleBases", "R2_AdapterBases", "R2_SampleBases", "# Reads"
-  )
-  assertthat::assert_that(all(colnames(d) == old_nms))
-  d |>
-    dplyr::rename(
-      index1 = "index", n_reads = "# Reads", SampleID = "Sample_ID", lane = "Lane"
-    ) |>
-    dplyr::mutate(barcode = ifelse(
-      is.na(.data$index1), NA_character_, glue("{.data$index1}-{.data$index2}")
-    )) |>
-    dplyr::select(
-      "lane", "SampleID", "barcode", "n_reads",
-      "R1_AdapterBases", "R2_AdapterBases",
-      "R1_SampleBases", "R2_SampleBases"
-    )
-}
-
-#' BCLConvert Index Hopping Counts
-#'
-#' Reads the `Index_Hopping_Counts.csv` file in the `Reports` directory
-#' output by BCLConvert.
-#'
-#' @param x Path to `Index_Hopping_Counts.csv` file.
-#'
-#' @return Tibble
-#'
-#' @examples
-#' \dontrun{
-#' x <- here::here("nogit/bcl_convert/WGS_TsqNano/Reports/Index_Hopping_Counts.csv")
-#' bclconvert_read_indexhoppingcounts(x)
-#' }
-#' @export
-bclconvert_read_indexhoppingcounts <- function(x) {
-  d <- readr::read_csv(x, col_types = "ccccd")
-  old_nms <- c("Lane", "SampleID", "index", "index2", "# Reads")
-  assertthat::assert_that(all(colnames(d) == old_nms))
-  d |>
-    dplyr::rename(index1 = "index", n_reads = "# Reads", lane = "Lane") |>
-    dplyr::mutate(barcode = glue("{.data$index1}-{.data$index2}")) |>
-    dplyr::select("lane", "SampleID", "barcode", "n_reads")
-}
-
-#' BCLConvert Demultiplex Stats
-#'
-#' Reads the `Demultiplex_Stats.csv` file in the `Reports` directory
-#' output by BCLConvert.
-#'
-#' @param x Path to `Demultiplex_Stats.csv` file.
-#'
-#' @return Tibble
-#'
-#' @examples
-#' \dontrun{
-#' x <- here::here("nogit/bcl_convert/WGS_TsqNano/Reports/Demultiplex_Stats.csv")
-#' bclconvert_read_demultiplexstats(x)
-#' }
-#' @export
-bclconvert_read_demultiplexstats <- function(x) {
-  nms <- tibble::tribble(
-    ~new_nm, ~old_nm, ~class,
-    "lane", "Lane", "c",
-    "SampleID", "SampleID", "c",
-    "barcode", "Index", "c",
-    "n_reads", "# Reads", "d",
-    "n_perfect_idxreads", "# Perfect Index Reads", "d",
-    "n_one_mismatch_idxreads", "# One Mismatch Index Reads", "d",
-    "n_q30_bases", "# of >= Q30 Bases (PF)", "d",
-    "mean_quality_score", "Mean Quality Score (PF)", "d"
-  )
-  lookup <- tibble::deframe(nms[c("new_nm", "old_nm")])
-  d <- readr::read_csv(x, col_types = nms[["class"]])
-  assertthat::assert_that(all(colnames(d) == nms[["old_nm"]]))
-  d |>
-    dplyr::rename(dplyr::all_of(lookup))
-}
