733476843ce0937e4568dc45782c26fa978406cf
pdiakumis <peterdiakumis@gmail.com>
2024-10-14 23:58:33 +1100

    dragen: generalise coverage metrics

--- R/tso_dragen.R
+++ R/tso_dragen.R
@@ -7,12 +7,13 @@
 #' \dontrun{
 #'
 #' #---- Local ----#
+#' prefix <- "L2401290"
 #' p <- file.path(
 #'   "~/s3/pipeline-prod-cache-503977275616-ap-southeast-2/byob-icav2/production",
-#'   "analysis/cttsov2/20240915ff0295ed"
+#'   "analysis/cttsov2/20240915ff0295ed/Logs_Intermediates/DragenCaller",
+#'   prefix
 #' )
-#' prefix <- "L2401290"
-#' t1 <- Wf_tso_ctdna_tumor_only_v2$new(path = p, prefix = prefix)
+#' t1 <- Wf_dragen$new(path = p, prefix = prefix)
 #' t1$list_files(max_files = 100)
 #' t1$list_files_filter_relevant(max_files = 300)
 #' d <- t1$download_files(max_files = 100, dryrun = F)
@@ -40,40 +41,40 @@ Wf_dragen <- R6::R6Class(
       pref <- prefix
       reg1 <- tibble::tribble(
         ~regex, ~fun,
-        glue("{dc}/{pref}\\-replay\\.json$"), "replay",
-        glue("{dc}/{pref}\\.cnv_metrics.csv$"), "DOWNLOAD_ONLY",
-        glue("{dc}/{pref}\\.exon_contig_mean_cov\\.csv$"), "contigMeanCov",
-        glue("{dc}/{pref}\\.exon_coverage_metrics\\.csv$"), "DOWNLOAD_ONLY",
-        glue("{dc}/{pref}\\.exon_fine_hist\\.csv$"), "DOWNLOAD_ONLY",
-        glue("{dc}/{pref}\\.exon_hist\\.csv$"), "DOWNLOAD_ONLY",
-        glue("{dc}/{pref}\\.exon_overall_mean_cov\\.csv$"), "DOWNLOAD_ONLY",
-        glue("{dc}/{pref}\\.fastqc_metrics\\.csv$"), "DOWNLOAD_ONLY",
-        glue("{dc}/{pref}\\.fragment_length_hist\\.csv$"), "DOWNLOAD_ONLY",
-        glue("{dc}/{pref}\\.gc_metrics\\.csv$"), "DOWNLOAD_ONLY",
-        glue("{dc}/{pref}\\.gvcf_metrics\\.csv$"), "DOWNLOAD_ONLY",
-        glue("{dc}/{pref}\\.mapping_metrics\\.csv$"), "DOWNLOAD_ONLY",
-        glue("{dc}/{pref}\\.microsat_diffs\\.txt$"), "DOWNLOAD_ONLY",
-        glue("{dc}/{pref}\\.microsat_output\\.json$"), "DOWNLOAD_ONLY",
-        glue("{dc}/{pref}\\.sv_metrics\\.csv$"), "DOWNLOAD_ONLY",
-        glue("{dc}/{pref}\\.target_bed_contig_mean_cov\\.csv$"), "contigMeanCov",
-        glue("{dc}/{pref}\\.target_bed_coverage_metrics\\.csv$"), "DOWNLOAD_ONLY",
-        glue("{dc}/{pref}\\.target_bed_fine_hist\\.csv$"), "DOWNLOAD_ONLY",
-        glue("{dc}/{pref}\\.target_bed_hist\\.csv$"), "DOWNLOAD_ONLY",
-        glue("{dc}/{pref}\\.target_bed_overall_mean_cov\\.csv$"), "DOWNLOAD_ONLY",
-        glue("{dc}/{pref}\\.time_metrics\\.csv$"), "DOWNLOAD_ONLY",
-        glue("{dc}/{pref}\\.tmb_contig_mean_cov\\.csv$"), "contigMeanCov",
-        glue("{dc}/{pref}\\.tmb_coverage_metrics\\.csv$"), "DOWNLOAD_ONLY",
-        glue("{dc}/{pref}\\.tmb_fine_hist\\.csv$"), "DOWNLOAD_ONLY",
-        glue("{dc}/{pref}\\.tmb_hist\\.csv$"), "DOWNLOAD_ONLY",
-        glue("{dc}/{pref}\\.tmb_overall_mean_cov\\.csv$"), "DOWNLOAD_ONLY",
-        glue("{dc}/{pref}\\.trimmer_metrics\\.csv$"), "DOWNLOAD_ONLY",
-        glue("{dc}/{pref}\\.umi_metrics\\.csv$"), "DOWNLOAD_ONLY",
-        glue("{dc}/{pref}\\.vc_metrics\\.csv$"), "DOWNLOAD_ONLY",
-        glue("{dc}/{pref}\\.wgs_contig_mean_cov\\.csv$"), "contigMeanCov",
-        glue("{dc}/{pref}\\.wgs_coverage_metrics\\.csv$"), "DOWNLOAD_ONLY",
-        glue("{dc}/{pref}\\.wgs_fine_hist\\.csv$"), "DOWNLOAD_ONLY",
-        glue("{dc}/{pref}\\.wgs_hist\\.csv$"), "DOWNLOAD_ONLY",
-        glue("{dc}/{pref}\\.wgs_overall_mean_cov\\.csv$"), "DOWNLOAD_ONLY"
+        glue("{pref}\\-replay\\.json$"), "replay",
+        glue("{pref}\\.cnv_metrics.csv$"), "DOWNLOAD_ONLY",
+        glue("{pref}\\.exon_contig_mean_cov\\.csv$"), "contigMeanCov",
+        glue("{pref}\\.exon_coverage_metrics\\.csv$"), "DOWNLOAD_ONLY",
+        glue("{pref}\\.exon_fine_hist\\.csv$"), "DOWNLOAD_ONLY",
+        glue("{pref}\\.exon_hist\\.csv$"), "DOWNLOAD_ONLY",
+        glue("{pref}\\.exon_overall_mean_cov\\.csv$"), "DOWNLOAD_ONLY",
+        glue("{pref}\\.fastqc_metrics\\.csv$"), "DOWNLOAD_ONLY",
+        glue("{pref}\\.fragment_length_hist\\.csv$"), "DOWNLOAD_ONLY",
+        glue("{pref}\\.gc_metrics\\.csv$"), "DOWNLOAD_ONLY",
+        glue("{pref}\\.gvcf_metrics\\.csv$"), "DOWNLOAD_ONLY",
+        glue("{pref}\\.mapping_metrics\\.csv$"), "DOWNLOAD_ONLY",
+        glue("{pref}\\.microsat_diffs\\.txt$"), "DOWNLOAD_ONLY",
+        glue("{pref}\\.microsat_output\\.json$"), "DOWNLOAD_ONLY",
+        glue("{pref}\\.sv_metrics\\.csv$"), "DOWNLOAD_ONLY",
+        glue("{pref}\\.target_bed_contig_mean_cov\\.csv$"), "contigMeanCov",
+        glue("{pref}\\.target_bed_coverage_metrics\\.csv$"), "DOWNLOAD_ONLY",
+        glue("{pref}\\.target_bed_fine_hist\\.csv$"), "DOWNLOAD_ONLY",
+        glue("{pref}\\.target_bed_hist\\.csv$"), "DOWNLOAD_ONLY",
+        glue("{pref}\\.target_bed_overall_mean_cov\\.csv$"), "DOWNLOAD_ONLY",
+        glue("{pref}\\.time_metrics\\.csv$"), "DOWNLOAD_ONLY",
+        glue("{pref}\\.tmb_contig_mean_cov\\.csv$"), "contigMeanCov",
+        glue("{pref}\\.tmb_coverage_metrics\\.csv$"), "DOWNLOAD_ONLY",
+        glue("{pref}\\.tmb_fine_hist\\.csv$"), "DOWNLOAD_ONLY",
+        glue("{pref}\\.tmb_hist\\.csv$"), "DOWNLOAD_ONLY",
+        glue("{pref}\\.tmb_overall_mean_cov\\.csv$"), "DOWNLOAD_ONLY",
+        glue("{pref}\\.trimmer_metrics\\.csv$"), "DOWNLOAD_ONLY",
+        glue("{pref}\\.umi_metrics\\.csv$"), "DOWNLOAD_ONLY",
+        glue("{pref}\\.vc_metrics\\.csv$"), "DOWNLOAD_ONLY",
+        glue("{pref}\\.wgs_contig_mean_cov\\.csv$"), "contigMeanCov",
+        glue("{pref}\\.wgs_coverage_metrics\\.csv$"), "DOWNLOAD_ONLY",
+        glue("{pref}\\.wgs_fine_hist\\.csv$"), "DOWNLOAD_ONLY",
+        glue("{pref}\\.wgs_hist\\.csv$"), "DOWNLOAD_ONLY",
+        glue("{pref}\\.wgs_overall_mean_cov\\.csv$"), "DOWNLOAD_ONLY"
       )
       regexes <- reg1 |>
         dplyr::mutate(
@@ -117,11 +118,12 @@ Wf_dragen <- R6::R6Class(
     },
     #' @description Read `contig_mean_cov.csv` file.
     #' @param x Path to file.
-    read_contigMeanCov = function(x) {
+    #' @param keep_alt Keep ALT contigs.
+    read_contigMeanCov = function(x, keep_alt = FALSE) {
       readr::read_csv(x, col_names = c("chrom", "n_bases", "coverage"), col_types = "cdd") |>
         dplyr::filter(
           if (!keep_alt) {
-            !grepl("chrM|MT|_|Autosomal|HLA-|EBV", .data$chrom)
+            !grepl("chrM|MT|_|Autosomal|HLA-|EBV|GL|hs37d5", .data$chrom)
           } else {
             TRUE
           }
@@ -130,24 +132,26 @@ Wf_dragen <- R6::R6Class(
     #' @description Read `dragen.tsv.gz` cancer report hrd file.
     #' @param x Path to file.
     read_coverageMetrics = function(x) {
-      abbrev_nm <- c(
-        "Aligned bases"                                       = "bases_aligned_dragen",
-        "Aligned bases in genome"                             = "bases_aligned_in_genome_dragen",
-        "Average alignment coverage over genome"              = "cov_alignment_avg_over_genome_dragen",
-        "Uniformity of coverage (PCT > 0.2*mean) over genome" = "cov_uniformity_over_genome_pct_gt02mean_dragen",
-        "Uniformity of coverage (PCT > 0.4*mean) over genome" = "cov_uniformity_over_genome_pct_gt04mean_dragen",
-        "Average chr X coverage over genome"                  = "cov_avg_x_over_genome_dragen",
-        "Average chr Y coverage over genome"                  = "cov_avg_y_over_genome_dragen",
-        "Average mitochondrial coverage over genome"          = "cov_avg_mt_over_genome_dragen",
-        "Average autosomal coverage over genome"              = "cov_avg_auto_over_genome_dragen",
-        "Median autosomal coverage over genome"               = "cov_median_auto_over_genome_dragen",
-        "Mean/Median autosomal coverage ratio over genome"    = "cov_mean_median_auto_ratio_over_genome_dragen",
-        "Aligned reads"                                       = "reads_aligned_dragen",
-        "Aligned reads in genome"                             = "reads_aligned_in_genome_dragen"
+      # all rows except 'Aligned bases' and 'Aligned reads' refer to the region
+      abbrev_nm <- tibble::tribble(
+        ~raw, ~clean, ~region,
+        "Aligned bases", "bases_aligned_tot_dragen", FALSE,
+        "Aligned reads", "reads_aligned_tot_dragen", FALSE,
+        "Aligned bases in ", "bases_aligned_", TRUE,
+        "Average alignment coverage over ", "cov_alignment_avg_over_", TRUE,
+        "Uniformity of coverage (PCT > 0.2*mean) over ", "cov_uniformity_pct_gt02mean_", TRUE,
+        "Uniformity of coverage (PCT > 0.4*mean) over ", "cov_uniformity_pct_gt04mean_", TRUE,
+        "Average chr X coverage over ", "cov_avg_x_over_", TRUE,
+        "Average chr Y coverage over ", "cov_avg_y_over_", TRUE,
+        "Average mitochondrial coverage over ", "cov_avg_mt_over_", TRUE,
+        "Average autosomal coverage over ", "cov_avg_auto_over_", TRUE,
+        "Median autosomal coverage over ", "cov_median_auto_over_", TRUE,
+        "Mean/Median autosomal coverage ratio over ", "cov_mean_median_auto_ratio_over_", TRUE,
+        "Aligned reads in ", "reads_aligned_in_", TRUE
       )
       raw <- readr::read_lines(x)
       assertthat::assert_that(grepl("COVERAGE SUMMARY", raw[1]))
-
+      # first detect if this is genome, QC coverage region, or target region
       res <- raw |>
         tibble::as_tibble_col(column_name = "value") |>
         tidyr::separate_wider_delim(
@@ -155,22 +159,50 @@ Wf_dragen <- R6::R6Class(
           delim = ",", too_few = "align_start",
           names = c("category", "dummy1", "var", "value", "pct")
         )
+      reg1 <- NULL
+      str1 <- NULL
+      tmp <- res |>
+        dplyr::filter(grepl("PCT of .* with coverage ", .data$var)) |>
+        dplyr::slice_head(n = 1) |>
+        dplyr::pull("var")
+      assertthat::assert_that(length(tmp) == 1)
+      if (grepl("genome", tmp)) {
+        str1 <- "genome"
+        reg1 <- "genome"
+      } else if (grepl("QC coverage region", tmp)) {
+        str1 <- "QC coverage region"
+        reg1 <- "qccovreg"
+      } else if (grepl("target region", tmp)) {
+        str1 <- "target region"
+        reg1 <- "targetreg"
+      } else {
+        cli::cli_abort("Cannot determine the coverage region from: {x}")
+      }
+      abbrev_nm <- abbrev_nm |>
+        dplyr::mutate(
+          raw = ifelse(.data$region, glue("{.data$raw}{str1}"), .data$raw),
+          clean = ifelse(.data$region, glue("{.data$clean}{reg1}_dragen"), .data$clean)
+        ) |>
+        dplyr::select("raw", "clean") |>
+        tibble::deframe()
       # split to rename the
       # "PCT of genome with coverage [100x: inf)" values
+      pat <- glue("PCT of {str1} with coverage ")
       res1 <- res |>
-        # pct just shows 100% for a couple rows
-        dplyr::filter(!grepl("PCT of genome with coverage", .data$var)) |>
+        # pct just shows % for a couple rows which can be
+        # calculated from their above values
+        dplyr::filter(!grepl(pat, .data$var)) |>
         dplyr::select("var", "value")
       res2 <- res |>
-        dplyr::filter(grepl("PCT of genome with coverage", .data$var)) |>
+        dplyr::filter(grepl(pat, .data$var)) |>
         dplyr::mutate(
-          var = sub("PCT of genome with coverage ", "", .data$var),
+          var = sub(pat, "", .data$var),
           var = gsub("\\[|\\]|\\(|\\)| ", "", .data$var),
           var = gsub("x", "", .data$var),
           var = gsub("inf", "Inf", .data$var)
         ) |>
         tidyr::separate_wider_delim("var", names = c("start", "end"), delim = ":") |>
-        dplyr::mutate(var = as.character(glue("cov_genome_pct_{start}_{end}_dragen"))) |>
+        dplyr::mutate(var = as.character(glue("cov_pct_{start}_{end}_{region}_dragen"))) |>
         dplyr::select("var", "value")
       res <- dplyr::bind_rows(res1, res2) |>
         dplyr::mutate(
@@ -180,6 +212,6 @@ Wf_dragen <- R6::R6Class(
         ) |>
         tidyr::pivot_wider(names_from = "var", values_from = "value")
       return(res)
-    },
+    }
   ) # end public
 ) # end Wf_dragen
