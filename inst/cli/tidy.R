87086270c062325b549091f57d7718b78da615d7
pdiakumis <peterdiakumis@gmail.com>
2024-12-05 23:59:45 +1100

    cli/tidy.R: keep token placeholder for orcabus

--- inst/cli/tidy.R
+++ inst/cli/tidy.R
@@ -3,8 +3,8 @@ tidy_add_args <- function(subp) {
   tidy$add_argument("-i", "--in_dir", help = glue("{emoji('snowman')} Directory with untidy UMCCR workflow results. Can be GDS, S3 or local."), required = TRUE)
   tidy$add_argument("-o", "--out_dir", help = glue("{emoji('fire')} Directory to output tidy results."), required = TRUE)
   tidy$add_argument("-p", "--prefix", help = glue("{emoji('violin')} Prefix string used for all results."), required = TRUE)
-  tidy$add_argument("-t", "--token", help = glue("{emoji('see_no_evil')} ICA access token. Default: ICA_ACCESS_TOKEN env var."), default = Sys.getenv("ICA_ACCESS_TOKEN"))
-  tidy$add_argument("-l", "--local_dir", help = glue("{emoji('inbox_tray')} If input is a GDS/S3 directory, download the recognisable files to this directory. Default: '<out_dir>/dracarys_<gds|s3>_sync'."))
+  # tidy$add_argument("-t", "--token", help = glue("{emoji('see_no_evil')} ICA access token. Default: ICA_ACCESS_TOKEN env var."), default = Sys.getenv("ICA_ACCESS_TOKEN"))
+  tidy$add_argument("-l", "--local_dir", help = glue("{emoji('inbox_tray')} If input is an S3 directory, download the recognisable files to this directory. Default: '<out_dir>/dracarys_s3_sync'."))
   tidy$add_argument("-f", "--format", help = glue("{emoji('art')} Format of output. Default: %(default)s."), default = "tsv")
   tidy$add_argument("-n", "--dryrun", help = glue("{emoji('camel')} Dry run - just show files to be tidied."), action = "store_true")
   tidy$add_argument("-q", "--quiet", help = glue("{emoji('sleeping')} Shush all the logs."), action = "store_true")
@@ -22,7 +22,7 @@ tidy_parse_args <- function(args) {
     rds_dir <- normalizePath(args$rds_dir)
     rds_path <- file.path(rds_dir, glue("{args$prefix}_dracarys_data.rds"))
   }
-  token <- ica_token_validate(args$token)
+  # token <- ica_token_validate(args$token)
 
   tidy_args <- list(
     in_dir = args$in_dir,
@@ -31,7 +31,7 @@ tidy_parse_args <- function(args) {
     local_dir = local_dir,
     out_format = args$format,
     dryrun = args$dryrun,
-    token = token,
+    # token = token,
     pattern = args$pattern
   )
 
